bb3d0a513e67179f860d4b3da1aa9fb7e6eb0cfc
stephaniesaab <stephaniesaab5@gmail.com>
2026-04-02 13:26:59 -0400

    Added HWE and PCA

--- scripts/lactose_persistence.R
+++ scripts/lactose_persistence.R
@@ -71,8 +71,6 @@ keep_snps <- snp_sums > 0 & snp_sums < 2*nrow(geno_data) & snp_sums > 0.001*nrow
 geno_data_filt <- geno_data[, c(names(which(keep_snps)), "SampleID", "Population", "Superpopulation", "Sex")]
 genotypes_filt <- genotype_columns[, names(which(keep_snps))]
 
-#Filter for geographic locations that will likely show differences in lactose persistence based on literature review
-
 # HWE Filtering Function by Superpopulation ====
 # 1 = p^2 + 2pq + q^2
 #Identify SNPs that deviate from HWE within specific groups
@@ -140,7 +138,79 @@ length(hwe_fails) #Remove 679 SNPs
 geno_data_filt <- geno_data_filt %>% 
   select(-all_of(hwe_fails))
 
-# Check how many SNPs you have left
+# Check how many SNPs are left
 print(paste("Original SNPs:", ncol(genotype_columns)))
-print(paste("Informative SNPs remaining:", ncol(geno_data_filt %>% select(-SampleID, -Sex, -Population, -Superpopulation, ))))
+print(paste("Informative SNPs remaining:", ncol(geno_data_filt %>% select(-SampleID, -Sex, -Population, -Superpopulation))))
+
+#Filter for geographic locations that will likely show differences in lactose persistence based on literature review
+geno_matrix_small <- geno_data_filt %>% 
+  filter(Superpopulation %in% c("AFR", "EUR", "EAS")) %>% 
+  select(-SampleID, -Sex, -Population, -Superpopulation)
+
+#Labels for metadata
+labels_small <- geno_data_filt %>% 
+  filter(Superpopulation %in% c("AFR", "EUR", "EAS")) %>% 
+  select(SampleID, Sex, Population, Superpopulation)
+
+#Calculate the sum of alleles for each SNP
+# A sum of 0 means the variant (minor allele) is not present in the 3,202 samples
+snp_sums2 <- colSums(geno_matrix_small, na.rm = TRUE)
+
+#Identify SNPs with at least one variant (sum > 0)
+#Remove SNPs where everyone has the variant (sum == 2 * number of samples (3202))
+#Remove SNPs that are rare in the population (<0.001)
+keep_snps2 <- snp_sums2 > 0 & snp_sums2 < 2*nrow(geno_matrix_small) & snp_sums2 > 0.001*nrow(geno_matrix_small)
+
+#Subset data to keep only informative SNPs
+genotypes_small_filt <- geno_matrix_small[, names(which(keep_snps2))]
+print(paste("Informative SNPs remaining:", ncol(genotypes_small_filt))) #2086
+
+#Check Missing values
+sum(is.na(genotypes_small_filt)) #0
+
+rownames(genotypes_small_filt) <- labels_small$SampleID
+#PCA =====
+#Prepare numeric genotypes (Isolate only the SNP columns for calcs)
+geno_matrix <- genotypes_small_filt %>% 
+  as.matrix()
+
+#Normalization
+#Using scale. = TRUE in prcomp to standardize data
+#Scales each SNP to have unit variance (mean = 0, SD = 1)
+
+#Perform PCA
+pca_result <- prcomp(geno_matrix, center = TRUE, scale. = TRUE)
+#Summary results of PCA
+summary_pca <- summary(pca_result)
+
+#Variance explanation (Scree plot)
+pca_var <- pca_result$sdev^2
+var_explained <- pca_var / sum(pca_var)
+cumu_var <- cumsum(var_explained)
+
+#Figures for PCA: Scree plot
+plot(var_explained[1:10], type = "b",
+     main = "Figure 1: Scree Plot of LCT and MCM6 Region PCA",
+     xlab = "Principal Component",
+     ylab = "Proportion of Variance",
+     col = "blue", pch = 19)
+
+pca_df <- as.data.frame(pca_result$x)
+pca_df$Superpopulation <- labels_small$Superpopulation
+
+#Identifying top SNPs ====
+#Goal is to identify lactose persistence across genetic groups so want to colour PCA by superpopulation
+
+#PCA of lactose persistence region
+ggplot(pca_df, aes(x = PC1, y = PC2, color = Superpopulation)) +
+  geom_point(alpha = 0.7, size = 2) +
+  theme_minimal()+
+  labs(title = "Figure 2: PCA of Lactose Persistence by Region",
+       subtitle = "Samples clustered by superpopulation",
+       x = paste0("PC1 (", round(var_explained[1]*100, 1), "%)"),
+       y = paste0("PC2 (", round(var_explained[2]*100, 1), "%)"))
+
+
+#Identify top contributing SNPs (Loadings)
+snp_loadings <- data.frame()
 
